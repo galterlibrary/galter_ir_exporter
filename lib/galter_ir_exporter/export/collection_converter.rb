42a79a5f529bd610d7cb5fe8d10907b3d61b28e6
Austin <sharpattack@users.noreply.github.com>
2020-10-19 16:17:14 -0400

    [#2] Add Metadata in *Converter
    Add all data available from ActiveFedora object instance of GenericFile
    and Collection to *Converter. closes #2

--- lib/galter_ir_exporter/export/collection_converter.rb
+++ lib/galter_ir_exporter/export/collection_converter.rb
@@ -14,6 +14,34 @@ module GalterIrExporter
         @creator = collection.creator.map { |c| c }
         @members = collection.members.map(&:id)
         @permissions = permissions(collection)
+        @depositor = collection.depositor
+        @part_of = collection.part_of
+        @contributor = collection.contributor
+        @publisher = collection.publisher
+        @date_created = collection.date_created
+        @date_uploaded = collection.date_uploaded
+        @date_modified = collection.date_modified
+        @subject = collection.subject
+        @language = collection.language
+        @language = collection.language
+        @resource_type = collection.resource_type
+        @identifier = collection.identifier
+        @based_near = collection.based_near
+        @tag = collection.tag
+        @related_url = collection.related_url
+        @abstract = collection.abstract
+        @bibliographic_citation = collection.bibliographic_citation
+        @digital_origin = collection.digital_origin
+        @mesh = collection.mesh
+        @lcsh = collection.lcsh
+        @subject_geographic = collection.subject_geographic
+        @subject_name = collection.subject_name
+        @multi_page = collection.multi_page
+        @institutional_collection = collection.institutional_collection
+        @private_note = collection.private_note
+        @member_ids = collection.member_ids
+        @parent_id = collection.parent_id
+        @combined_file_id = collection.combined_file_id
       end
     end
   end
